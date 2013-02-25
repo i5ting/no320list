2563b0a7b9e5141363ea694085618b3cc6e47955
shiren1118 <shiren1118@126.com>
2013-02-25 21:58:29 +0800

    Initial Commit

---
+++ no320list/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  no320list
+//
+//  Created by sang alfred on 2/25/13.
+//  Copyright (c) 2013 sang alfred. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ no320list/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  no320list
+//
+//  Created by sang alfred on 2/25/13.
+//  Copyright (c) 2013 sang alfred. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
